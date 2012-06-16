bd2f0809166b4708d777d4793b5b5c476a35612a
ckirkendall <ckirkendall@gmail.com>
2012-06-16 07:18:30 -0400

    fixed several typos in the example code

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring "1.0.1"]
                  [enfocus "0.9.1-SNAPSHOT"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]])
+    :cljsbuild {
+      :builds [{
+         :source-path "src"
+                :compiler {:output-to "resources/public/cljs/bootstrap.js"
+                           :optimizations :advanced
+                           :pretty-print false}}]})
