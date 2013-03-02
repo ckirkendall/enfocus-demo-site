3a5ab2ab6332dfd211a20b9b83f4fd44b760efb7
ckirkendall <ckirkendall@gmail.com>
2013-03-02 08:47:30 -0500

    Moved site to 1.0.0-RC1

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject enfocus.demo "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.0.1"]
-                 [enfocus "1.0.0-alpha3"]]
+                 [enfocus "1.0.0-RC1"]]
+  :dev-dependencies [[lein-cljsbuild "0.2.10"]]
     :cljsbuild {
       :builds [{
          :source-path "src"
