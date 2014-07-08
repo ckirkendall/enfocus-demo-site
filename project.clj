830a1e073b50e5a5cdb90f03498d31984ac86d11
Creighton Kirkendall <ckirkendall@gmail.com>
2014-07-08 16:12:02 -0400

    clean up for the release of 2.1.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://ckirkendall.github.io/enfocus-site"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2234"]
-                 [enfocus "2.1.0-SNAPSHOT"]
+                 [enfocus "2.1.0"]
                  [ring/ring-core "1.1.8"]
                  [ring/ring-jetty-adapter "1.1.8"]]
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
