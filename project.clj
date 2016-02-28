348a860ad9caa6b1beeba59a2eeb2f8312f33db3
Creighton Kirkendall <ckirkendall@gmail.com>
2016-02-28 09:39:23 -0500

    fixed issue with old version of clojurescript

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject enfocus-demp "2.0.0-SNAPSHOT"
   :description "enfocus documentation site"
   :url "http://ckirkendall.github.io/enfocus-site"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2234"]
-                 [enfocus "2.1.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [enfocus "2.1.1"]
                  [ring/ring-core "1.1.8"]
                  [ring/ring-jetty-adapter "1.1.8"]]
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.1.2"]
             [lein-ring "0.8.3"]]
   :cljsbuild {:builds {:prod {:source-paths ["src"],
                               :compiler {:output-to "resources/public/js/bootstrap.js"
-                                         :optimizations :advanced
+                                         :optimizations :simple
                                          :externs ["resources/public/js/prettify.js" "resources/public/js/lang-clj.js"]
                                          :pretty-print false}}}})
